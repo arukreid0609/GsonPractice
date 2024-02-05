570c0878eda8943b1ebba5e9ec2603a99542ded9
sasaki <y.sasaki.roppongi@gmail.com>
2024-02-05 18:30:21 +0900

    使用例作成中

--- src/main/GsonMain.java
+++ src/main/GsonMain.java
@@ -9,15 +9,11 @@ public class GsonMain {
 	public static void main(String[] args) {
 		var g = new GsonSave<Human>("save.json");
 		List<Human> list = new ArrayList<Human>();
-		list.add(new Hero("勇者A",100));
+		list.add(new Hero("勇者AAA",100));
 		list.add(new Hero("ヒーローB",150));
 		list.add(new Wizard("魔法使いaaaaa",50));
 
 		g.save(list);
-		list = g.loadList();
-//		for(Human h:list) {
-//			System.out.println(h);
-//		}
 	}
 
 }
--- src/main/GsonMain2.java
+++ src/main/GsonMain2.java
@@ -1,19 +1,13 @@
 package main;
-import java.util.ArrayList;
-import java.util.List;
-
 import gsonsave.GsonSave;
 
 public class GsonMain2 {
 
 	public static void main(String[] args) {
-		var g = new GsonSave("save.json");
-		List<Human> list = new ArrayList<Human>();
-		list.add(new Hero("勇者A",100));
-		list.add(new Hero("ヒーローB",150));
-		list.add(new Wizard("魔法使いaaaaa",50));
+		GsonSave<Human> g = new GsonSave<Human>("save.json");
+		Hero hero = new Hero("勇者A",100);
 
-		g.save(list);
+		g.save(hero);
 //		list = g.loadHumans();
 //		for(Human h:list) {
 //			System.out.println(h);
